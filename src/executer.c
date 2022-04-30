ae284c9c64c598395b84ce2a0850564eb564aaa0
atedesch1 <alvaro_porc@hotmail.com>
2022-04-30 01:09:28 -0300

    implement pipe operator

--- src/executer.c
+++ src/executer.c
@@ -2,73 +2,114 @@
 
 void execute_command(program_call **program_calls, char **redirect_paths)
 {
-    bool is_first_program_call = true;
+    pid_t child_pid;
+
+    // saves default input/output
+    int def_input = dup(STDIN_FILENO);
+    int def_output = dup(STDOUT_FILENO);
+    int def_erroutput = dup(STDERR_FILENO);
+
+    int fd_input;
+    int fd_output;
+    int fd_erroutput;
+
+    // sets error output for any process
+    set_erroutput(def_erroutput, &fd_erroutput, redirect_paths[2]);
+    // redirect error output
+    dup2(fd_erroutput, STDERR_FILENO);
+    close(fd_erroutput);
+
+    // sets input for first process
+    set_input(def_input, &fd_input, redirect_paths[0]);
+
     for (program_call **p = program_calls; *p != NULL; p++)
     {
-        pid_t pid = fork();
-        if (pid < 0)
+        // redirect input
+        dup2(fd_input, STDIN_FILENO);
+        close(fd_input);
+
+        if (p[1] == NULL)
+        { // last process
+            // sets output for last process
+            set_output(def_output, &fd_output, redirect_paths[1]);
+        }
+        else
         {
-            perror("couldn't fork process");
-            exit(1);
+            int fdpipe[2];
+            if (pipe(fdpipe) == -1)
+            {
+                perror("couldn't create pipe");
+                exit(1);
+            }
+            fd_input = fdpipe[0]; // pipe output from current process to next
+            fd_output = fdpipe[1];
         }
 
-        if (pid == 0)
+        // redirect output
+        dup2(fd_output, STDOUT_FILENO);
+        close(fd_output);
+
+        child_pid = fork();
+        if (child_pid < 0)
+        {
+            perror("couldn't fork process");
+            exit(2);
+        }
+
+        if (child_pid == 0)
         { // child process
-            if (is_first_program_call && redirect_paths[0] != NULL)
-            {
-                redirect_input(redirect_paths[0]);
-                is_first_program_call = false;
-            }
-            if (p[1] == NULL && redirect_paths[1] != NULL) // if last program_call and redirect output is not NULL
-            {
-                redirect_output(redirect_paths[1]);
-            }
-            if (redirect_paths[2] != NULL)
-            {
-                redirect_erroutput(redirect_paths[2]);
-            }
-            if (execve((*p)->program, (*p)->params, (*p)->envparams) == -1) // execute program
+            if (execve((*p)->program, (*p)->params, (*p)->envparams) == -1)
             {
                 perror("couldn't execute program");
                 exit(3);
             }
         }
-        else
-        { // shell process
-            wait(NULL);
-        }
+    }
+
+    // restore input/output defaults
+    dup2(def_input, STDIN_FILENO);
+    close(def_input);
+    dup2(def_output, STDOUT_FILENO);
+    close(def_output);
+    dup2(def_erroutput, STDERR_FILENO);
+    close(def_erroutput);
+
+    // waits for last process
+    waitpid(child_pid, NULL, 0);
+}
+
+void set_input(int def_input, int *fd_input, char *redirect_input_path)
+{
+    if (redirect_input_path != NULL)
+    {
+        *fd_input = open(redirect_input_path, O_RDONLY);
+    }
+    else
+    {
+        *fd_input = dup(def_input);
     }
 }
 
-void redirect_input(char *input_path)
+void set_output(int def_output, int *fd_output, char *redirect_output_path)
 {
-    int fd = open(input_path, O_RDONLY);
-    if (fd == -1)
+    if (redirect_output_path != NULL)
     {
-        perror("couldn't open");
+        *fd_output = open(redirect_output_path, O_WRONLY | O_CREAT, RWX_PERMISSION_ALL);
     }
-    dup2(fd, STDIN_FILENO);
-    close(fd);
+    else
+    {
+        *fd_output = dup(def_output);
+    }
 }
 
-void redirect_output(char *output_path)
+void set_erroutput(int def_erroutput, int *fd_erroutput, char *redirect_erroutput_path)
 {
-    int fd = open(output_path, O_WRONLY | O_CREAT, 0777);
-    if (fd == -1)
+    if (redirect_erroutput_path != NULL)
     {
-        perror("couldn't open");
+        *fd_erroutput = open(redirect_erroutput_path, O_APPEND | O_WRONLY | O_CREAT, RWX_PERMISSION_ALL);
     }
-    dup2(fd, STDOUT_FILENO);
-    close(fd);
-}
-
-void redirect_erroutput(char *erroutput_path)
-{
-    int fd = open(erroutput_path, O_WRONLY | O_CREAT, 0777);
-    if (fd == -1)
+    else
     {
-        perror("couldn't open");
+        *fd_erroutput = dup(def_erroutput);
     }
-    dup2(fd, STDERR_FILENO);
-    close(fd);
 }
--- src/executer.h
+++ src/executer.h
@@ -11,16 +11,18 @@
 #ifndef EXECUTER
 #define EXECUTER
 
+#define RWX_PERMISSION_ALL 0777
+
 // executes command
 void execute_command(program_call **program_calls, char **redirect_paths);
 
-// redirects current process stdin to input_path file
-void redirect_input(char *input_path);
+// makes fd_input point to input file descriptor
+void set_input(int def_input, int *fd_input, char *redirect_input_path);
 
-// redirects current process stdout to output_path file
-void redirect_output(char *output_path);
+// makes fd_output point to output file descriptor
+void set_output(int def_output, int *fd_output, char *redirect_output_path);
 
-// redirects current process stderrout to erroutput_path file
-void redirect_erroutput(char *erroutput_path);
+// makes fd_erroutput point to error output file descriptor
+void set_erroutput(int def_erroutput, int *fd_erroutput, char *redirect_erroutput_path);
 
 #endif // !EXECUTER
